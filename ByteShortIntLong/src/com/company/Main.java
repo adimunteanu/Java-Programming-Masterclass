6db81d30ce799f658fe74b5e612692e8a88e173a
adimunteanu <adimunteanu13@gmail.com>
2020-03-08 16:02:40 +0100

    Primitive Data Types

---
+++ ByteShortIntLong/src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        int myMin = Integer.MIN_VALUE;
+        int myMax = Integer.MAX_VALUE;
+        System.out.println("Integer Min Val = " + myMin);
+        System.out.println("Integer Max Val = " + myMax);
+        byte myMinByte = Byte.MIN_VALUE;
+        byte myMaxByte = Byte.MAX_VALUE;
+        System.out.println("Byte Min Val = " + myMinByte);
+        System.out.println("Byte Max Val = " + myMaxByte);
+        long myMinLong = Long.MIN_VALUE;
+        long myMaxLong = Long.MAX_VALUE;
+        System.out.println("Long Min Val = " + myMinLong);
+        System.out.println("Long Max Val = " + myMaxLong);
+        short myMinShort = Short.MIN_VALUE;
+        short myMaxShort = Short.MAX_VALUE;
+        System.out.println("Short Min Val = " + myMinShort);
+        System.out.println("Short Max Val = " + myMaxShort);
+
+        float testDiv =  11 / 2;
+        System.out.println(testDiv);
+    }
+}
